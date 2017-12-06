3ead02417ed15d3a1b3a9d956655c1a76c7c2062
Maciek Chmura <chmura.m@gmail.com>
2017-12-06 11:05:28 +0100

    comments

--- 03.js
+++ 03.js
@@ -1,20 +1,31 @@
 function distanceInMemory(input) {
-  let spiral = [
-    [1, 0, 0, 0],
-    [2, 1, 0, 3],
-    [3, 1, 1, 4],
-    [4, 0, 1, 4],
+  //starting spiral
+  //each array represents: 
+  //[value, x coordinate, y coordinate, direction of "vector" to next value]
+  let spiral = [ //         2        value can have one 
+    [1, 0, 0, 0], //        ^        of four possible vectors
+    [2, 1, 0, 3], //  3 < value > 1
+    [3, 1, 1, 4], //        v
+    [4, 0, 1, 4], //        4
     [5, -1, 1, 4],
   ];
 
+  //generate next values and add to spiral with null coordinates
   for (let s = 6; s <= input; s++) {
     spiral.push([s, null, null, 0])
   }
 
+  //loop over spiral and calculate coordinates
   for (let i = 5; i < spiral.length; i++) {
     let curr = spiral[i];
     let prev = spiral[i - 1];
     let dir = prev[3];
+
+    //check the "vector" value to detemine where to look for empty space
+    //if there is space to put new value -> 
+    //add it by updating coord 
+    //and change vector
+    //if not create value in the same vector and move on
     if (dir === 4) {
       updateCoord(0, -1, 1, 0)
     } else if (dir === 1) {
